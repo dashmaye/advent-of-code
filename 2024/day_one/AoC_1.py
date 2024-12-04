8f0a3fa9eccdcb4ddcdb3fbb7675fdd55681abce
dashmaye <44510328+dashmaye@users.noreply.github.com>
2024-12-04 10:44:36 -0500

    Update AoC_1.py

--- 2024/day_one/AoC_1.py
+++ 2024/day_one/AoC_1.py
@@ -6,12 +6,14 @@ df = pd.read_excel('AoC_1.xlsx', header=None, sheet_name='CleanData')
 location_one = np.sort(df[0].to_numpy())
 location_two = np.sort(df[1].to_numpy())
 
+# part one to get distance
 distance = 0
 for loc in range(location_one.size):
     distance += abs(location_one[loc] - location_two[loc])
 
 print(distance)
 
+# part two to get similarity
 similarity = 0
 for loc_a in range(location_one.size):
     occurence = 0
